5199ecadaab60f6e3a44ce9072b0c0988ff7f822
michael.rempel@gmx.net <michael.rempel@gmx.net>
2019-10-11 15:52:45 +0200

    mrem update

--- src/main/BaggageScanner/BaggageScanner.java
+++ src/main/BaggageScanner/BaggageScanner.java
@@ -1,9 +1,17 @@
 package BaggageScanner;
 
 
+import java.util.Map;
 
 public class BaggageScanner {
-    ProhibitedItems x = ProhibitedItems.kn_fe;
+    ProhibitedItems x = ProhibitedItems.knife;
+
+/*
+    private Map<ProhibitedItems, String> ProhibitedItemStrings = new Map<ProhibitedItems, String> ({
+            {"kn!fe",
+            "exp|os!ve",
+            "glock|7"
+    });*/
 
 
 }
--- src/main/BaggageScanner/ProhibitedItems.java
+++ src/main/BaggageScanner/ProhibitedItems.java
@@ -1,12 +1,13 @@
 package BaggageScanner;
 
 public enum ProhibitedItems {
-    kn!fe,
-    exp|os!ve,
-    glock|7
+    knife,
+    explosive,
+    gun
 
     //Idee für Sonderzeichen geht nicht
  //   kn_fe("kn!fe"),
  //   exp_os_ve("exp|os!ve"),
     //glock|7,
 }
+
