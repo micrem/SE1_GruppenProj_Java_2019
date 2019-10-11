0c49f6646484f34d164fbeff4b841810e993f0f2
Kragu <51914757+Kragu@users.noreply.github.com>
2019-10-11 18:28:47 +0200

    Create Technician.java

---
+++ src/main/Employees/Technician.java
@@ -0,0 +1,9 @@
+package Employees;
+
+import java.util.Date;
+
+public class Technician extends Employee {
+    public Technician(int pid, String pname, Date pbirthDate) {
+        super(pid, pname, pbirthDate);
+    }
+}
