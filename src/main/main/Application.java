0ffc9da1047cacdc12a6b3a1877d7ace4859761d
michael.rempel@gmx.net <michael.rempel@gmx.net>
2019-10-11 18:41:49 +0200

    mrem update 2

--- src/main/main/Application.java
+++ src/main/main/Application.java
@@ -1,10 +1,47 @@
 package main;
 
+import BaggageScanner.BaggageScanner;
 import configuration.Configuration;
+
+import java.net.PasswordAuthentication;
 
 public class Application {
     // Festlegung dea Algorithmus zur such nach verboten Gegenstaende
     Configuration config  = Configuration.KnuthMorrisPratt;
 
     // Gepäkscännner legt seiene Objekte an
+
+    //read all data -> 3 Maps:
+        //map id - passagierObject
+        //map passangerID - handgepäckID
+        //map
+
+    //initialize all needed vars/classes
+        //new employees+cards etc
+        //new scanner
+        //set scanner state
+        //register users
+        //start machine
+        //change to "ON" state
+
+    //ON state
+    BaggageScanner scannerA = new BaggageScanner();
+
+    Passagier passagier1 = new Passagier();
+
+    passagier1.putBaggageInTray(scannerA );
+
+
+    loop passagiersArray into passagier
+            passagier.putBaggageInTray(checkpoint);
+    end loop;
+
+
+    checkpoint.checkPassagiers(passagierArray);
+
+    InterfaceScanner scanner = new BaggageScanner();
+
+    scanner.
+
+
 }
