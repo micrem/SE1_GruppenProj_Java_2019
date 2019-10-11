60c40cdac1536c95c78ff212d67d86e7065472d0
Kragu <51914757+Kragu@users.noreply.github.com>
2019-10-11 18:28:53 +0200

    Create FederalPoliceOfficer.java

---
+++ src/main/Employees/FederalPoliceOfficer.java
@@ -0,0 +1,10 @@
+package Employees;
+
+import java.util.Date;
+
+public class FederalPoliceOfficer extends Employee {
+
+    public FederalPoliceOfficer(int pid, String pname, Date pbirthDate) {
+        super(pid, pname, pbirthDate);
+    }
+}
