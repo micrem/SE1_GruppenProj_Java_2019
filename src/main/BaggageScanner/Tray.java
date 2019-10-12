078bd2250b505f03d45aac05a67757050b99b528
michael.rempel@gmx.net <michael.rempel@gmx.net>
2019-10-12 17:03:11 +0200

    add "none" to bad items enum

--- src/main/BaggageScanner/Tray.java
+++ src/main/BaggageScanner/Tray.java
@@ -1,7 +1,19 @@
 package BaggageScanner;
 
+import Passenger_Baggage.HandBaggage;
+
 public class Tray {
     private BaggageScanner baggageScanner;
+
+    private HandBaggage baggage;
+
+    public HandBaggage getBaggage() {
+        return baggage;
+    }
+
+    public void setBaggage(HandBaggage baggage) {
+        this.baggage = baggage;
+    }
 
     public Tray(BaggageScanner baggageScanner){
         this.baggageScanner = baggageScanner;
