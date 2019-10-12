dfcb77ce33b9c6c9c6c91afd20a9c47ee80ff98d
StefanBrand42 <56312367+StefanBrand42@users.noreply.github.com>
2019-10-12 14:46:27 +0200

    String zerlegung Layer und 1 zu 5 verküpfung

--- src/main/Passenger_Baggage/Layer.java
+++ src/main/Passenger_Baggage/Layer.java
@@ -3,8 +3,15 @@ package Passenger_Baggage;
 public class Layer {
 
     private String layerText;
+    private HandBaggage handBaggage;
 
-    public Layer(String layerText){
+    public Layer(String layerText, HandBaggage handBaggage){
         this.layerText = layerText;
+        this.handBaggage =handBaggage;
+        handBaggage.setLayer1(this);
+        handBaggage.setLayer2(this);
+        handBaggage.setLayer3(this);
+        handBaggage.setLayer4(this);
+        handBaggage.setLayer5(this);
     }
 }
